5fb3aeed0905682da6eb6be41fbb351bab539a2e
francns <franc.ns@gmail.com>
2019-10-15 15:40:05 +0200

    added playing song's and artists' names with js

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// playing song and artists
+document.getElementById('playing-song').innerHTML = 'This Is Me'
+document.getElementById('playing-artist-1').innerHTML = 'Keala Settle,'
+document.getElementById('playing-artist-2').innerHTML = 'The Greatest Showman Ensemble'
